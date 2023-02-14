1b60b4a39ee7a21e1efc27fa26773bf1fed9ff64
Sara <112416629+sarak612@users.noreply.github.com>
2023-02-14 17:32:29 +0200

    Add files via upload

---
+++ Week6Day3DaillyChallenge.py
@@ -0,0 +1,31 @@
+# Challenge 1
+# Ask the user for a number and a length.
+# Create a program that prints a list of multiples of the number until the list length reaches length.
+
+# number = int(input("Enter a number: "))
+# length = int(input("Enter a length: "))
+# print("The multiples are: ")
+# for i in range(length):
+#     print(number*(i+1), end =" ")
+
+# Challenge 2
+# Write a program that asks a string to the user, and display a new string with
+# any duplicate consecutive letters removed.
+def remove_consec_letter_duplicates(word):
+    result_word = ""
+    prev_char = ""
+    for char in word:
+        if len(result_word) == 0:
+            result_word += char
+            prev_char = char
+        if char == prev_char:
+            continue
+        else:
+            result_word += char
+            prev_char = char
+    return result_word
+
+word = input("Enter a word")
+print(word)
+word = remove_consec_letter_duplicates(word)
+print(word)
